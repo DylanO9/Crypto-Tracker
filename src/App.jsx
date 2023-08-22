1c668daed78c46b86cc746e6dc3f3e54e3dabd8a
dolivares11 <dolivares@ucsd.edu>
2023-08-21 21:00:34 -0700

    Feature: Search Bar coin lookup, coin card class
    I made a class that can dynamically create cards based on the object passed through. Search bar look up will be able to add coins to your favorites list

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import './styles.css';
 import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
+import Coin from './coin';
 import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
-
-
-const app = initializeApp(firebaseConfig);
-
-const url = 'https://coingecko.p.rapidapi.com/coins/list';
+const url1 = 'https://coingecko.p.rapidapi.com/coins/'
+const url2 = '?localization=true&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false';
 const options = {
 	method: 'GET',
 	headers: {
@@ -17,27 +16,44 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+const app = initializeApp(firebaseConfig);
 
 function App() {
+	const [coin, setCoin] = useState({});
+	const [favorites, setFavorites] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [coinFound, setCoinFound] = useState(false);
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log('Search Term: ' + searchTerm)
+		const response = await fetch(url1 + searchTerm + url2, options);
+		console.log(url1 + searchTerm + url2, options)
+		const data = await response.json();
+		if(JSON.stringify(data) === '{"error":"coin not found"}') {
+			console.log("No data found");
+			setCoin({});
+			setCoinFound(false);
+		} else {
+			setCoin(data);
+			console.log(data);
+			console.log("Form submitted");
+			setCoinFound(true);
+		}
+	};
     
     return (
         <div className='App'>
 			<header>
 				<h1 className='dashboard'>Dashboard</h1>
-				<input type='text' placeholder='Search' className='search'/>
+				<form onSubmit={handleSubmit}>
+						<input type='text' placeholder='Search' id='search' value={searchTerm} onChange={e =>{setSearchTerm(e.target.value); console.log(e.target.value);}}/>
+				</form>
 			</header>
 
 			<main>
 				<div className='sidebar'>
-					
+
 				</div>
 				<div className='data'>
 					<div className='top'>
@@ -55,6 +71,9 @@ function App() {
 						<div className='favorites'>
 							<h1 className='favorite-title'>Favorites</h1>
 							<div className='divider'></div>
+							{coinFound === true ? <Coin {...coin}/> 
+								: <h1 className='no-id'>No data found</h1>
+							}
 						</div>
 
 						<div className='graph'>
--- src/Coin.jsx
+++ src/Coin.jsx
@@ -1,7 +1,29 @@
+/* eslint-disable react/prop-types */
 function Coin (props) {
 // Props will pass the name of the coin
 // use the API to fetch the data for the coin
 // display the data for the coin
+const image = props.image;
+const price = props.market_data.current_price.usd;
+const priceChange_raw = props.market_data.price_change_percentage_7d;
+const priceChange = parseInt(priceChange_raw.toString(), 10);
+console.log('thumb: ' + image.thumb);
+    return (
+        <>
+            <div className='coin' key={props.id}>
+                <img src={image.small} alt='' className='coin-image'></img>
+                <div className='mid-coin'>
+                    <h1 className='coin-name'>{props.name}</h1>
+                    <h1 className='coin-symbol'>{props.symbol}</h1>
+                </div>
+                <div className='side-coin'>
+                    <h1 className='current-price'>${price}</h1>
+                    <h1 className="price-change-7-days">{priceChange}%</h1>
+                </div>
+            </div>
+            <div className='divider'></div>
+        </>
+    )
 }
 
 export default Coin;
