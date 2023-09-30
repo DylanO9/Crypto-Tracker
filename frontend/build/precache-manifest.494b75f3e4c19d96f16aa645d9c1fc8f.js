c2afaa4420910147d81dcb53699e8350cff621c6
dolivares11 <dolivares@ucsd.edu>
2023-09-29 19:31:18 -0700

    Fix hosting

---
+++ frontend/build/precache-manifest.494b75f3e4c19d96f16aa645d9c1fc8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeea24eac4074c53cd81",
+    "url": "/static/css/main.05c4783d.chunk.css"
+  },
+  {
+    "revision": "aeea24eac4074c53cd81",
+    "url": "/static/js/main.cea1afd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cff0a1feb98c8dc96cd8",
+    "url": "/static/js/2.643bf1cf.chunk.js"
+  },
+  {
+    "revision": "fdba8c18e2b53de9c1b643e445a43db2",
+    "url": "/index.html"
+  }
+];
